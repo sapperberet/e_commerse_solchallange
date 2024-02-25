22f89ca223492c8661e64a2750d0a529518ecef2
sapperberet <ahmedamromran2003@gmail.com>
2024-02-25 23:25:02 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:e_commerse_solchallange/screens/splash/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+
+import 'routes.dart';
+import 'theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'hotelino',
+      theme: AppTheme.lightTheme(context),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
